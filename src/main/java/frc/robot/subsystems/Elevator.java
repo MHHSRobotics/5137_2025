5a4c8b78c408d3b145356b47d1ecebd29779ad86
npasetto0 <blasphemorgulus@gmail.com>
2025-01-11 14:47:44 -0800

    Changes

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -11,9 +11,12 @@ import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.sim.TalonFXSimState;
 
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.units.measure.MutAngle;
 import edu.wpi.first.units.measure.MutAngularVelocity;
 import edu.wpi.first.units.measure.MutDistance;
@@ -21,51 +24,63 @@ import edu.wpi.first.units.measure.MutLinearVelocity;
 import edu.wpi.first.units.measure.MutVelocity;
 import edu.wpi.first.units.measure.MutVoltage;
 import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+import edu.wpi.first.wpilibj.simulation.EncoderSim;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.constants.ElevatorConstants;
 
 public class Elevator extends SubsystemBase{
+
     private TalonFX leftMotor;
     private TalonFX rightMotor;
-    private final DutyCycleOut request = new DutyCycleOut(0.0);
-    private PIDController controller;
-    private ElevatorFeedforward feedforward;
-    private double goal = 0;
-    private boolean manualControl;
-    // Mutable holder for unit-safe voltage values, persisted to avoid reallocation.
-  private final MutVoltage appliedVoltage = Volts.mutable(0);
-  // Mutable holder for unit-safe linear distance values, persisted to avoid reallocation.
-  private final MutDistance pos = Meters.mutable(0);
-  // Mutable holder for unit-safe linear velocity values, persisted to avoid reallocation.
-  private final MutLinearVelocity velocity = MetersPerSecond.mutable(0);
 
-  // Create a new SysId routine for characterizing the shooter.
-  public final SysIdRoutine sysIdRoutine = 
-      new SysIdRoutine(
-          // Empty config defaults to 1 volt/second ramp rate and 7 volt step voltage.
-          new SysIdRoutine.Config(),
-          new SysIdRoutine.Mechanism(
-              // Tell SysId how to plumb the driving voltage to the motor(s).
-              this::setVoltage,
-              // Tell SysId how to record a frame of data for each motor on the mechanism being
-              // characterized.
-              log -> {
-                // Record a frame for the shooter motor.
-                log.motor("elevator")
-                    .voltage(
-                        appliedVoltage.mut_replace(
-                            leftMotor.get() * RobotController.getBatteryVoltage(), Volts))
-                    .linearPosition(pos.mut_replace(getMeasurement(), Meters))
-                    .linearVelocity(
-                        velocity.mut_replace(getVelocity(), MetersPerSecond));
-              },
-              // Tell SysId to make generated commands require this subsystem, suffix test state in
-              // WPILog with this subsystem's name ("shooter")
-              this));
+    private final DutyCycleOut request = new DutyCycleOut(0.0);
+
+    private PIDController controller;
+
+    private ElevatorFeedforward feedforward;
+
+    private double goal = 0;
+
+    private boolean manualControl;
+
+    // Mutable holder for unit-safe voltage values, persisted to avoid reallocation.
+    private final MutVoltage appliedVoltage = Volts.mutable(0);
+    // Mutable holder for unit-safe linear distance values, persisted to avoid reallocation.
+    private final MutDistance pos = Meters.mutable(0);
+    // Mutable holder for unit-safe linear velocity values, persisted to avoid reallocation.
+    private final MutLinearVelocity velocity = MetersPerSecond.mutable(0);
+
+    // Create a new SysId routine for characterizing the shooter.
+    public final SysIdRoutine sysIdRoutine = 
+        new SysIdRoutine(
+            // Empty config defaults to 1 volt/second ramp rate and 7 volt step voltage.
+            new SysIdRoutine.Config(),
+            new SysIdRoutine.Mechanism(
+                // Tell SysId how to plumb the driving voltage to the motor(s).
+                this::setVoltage,
+                // Tell SysId how to record a frame of data for each motor on the mechanism being
+                // characterized.
+                log -> {
+                    // Record a frame for the shooter motor.
+                    log.motor("elevator")
+                        .voltage(
+                            appliedVoltage.mut_replace(
+                                leftMotor.get() * RobotController.getBatteryVoltage(), Volts))
+                        .linearPosition(pos.mut_replace(getMeasurement(), Meters))
+                        .linearVelocity(
+                            velocity.mut_replace(getVelocity(), MetersPerSecond));
+                },
+                // Tell SysId to make generated commands require this subsystem, suffix test state in
+                // WPILog with this subsystem's name ("shooter")
+                this));
+
     public Elevator(){
         controller = new PIDController(ElevatorConstants.kP,ElevatorConstants.kI,ElevatorConstants.kD);
         feedforward = new ElevatorFeedforward(ElevatorConstants.ks, ElevatorConstants.kg, ElevatorConstants.kv);
