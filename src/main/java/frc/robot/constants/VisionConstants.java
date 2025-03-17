e153374e938ba61289212cbe8bdb19759c59e944
npasetto0 <blasphemorgulus@gmail.com>
2025-03-17 16:14:20 -0700

    Changes to camera positions

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -6,8 +6,8 @@ import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 
 public class VisionConstants {
-    public static final Transform3d robotToBLATCamera = new Transform3d(new Translation3d(-0.3048,0.216,0.2667),new Rotation3d(0,Units.degreesToRadians(-10),Units.degreesToRadians(230)));
-    public static final Transform3d robotToBRATCamera = new Transform3d(new Translation3d(-0.3048,-0.216,0.2667),new Rotation3d(0,Units.degreesToRadians(-10),Units.degreesToRadians(-230)));
+    public static final Transform3d robotToBLATCamera = new Transform3d(new Translation3d(-0.3175,0.222,0.273),new Rotation3d(0,Units.degreesToRadians(-12),Units.degreesToRadians(231)));
+    public static final Transform3d robotToBRATCamera = new Transform3d(new Translation3d(-0.3175,-0.222,0.273),new Rotation3d(0,Units.degreesToRadians(-12),Units.degreesToRadians(-231)));
     public static final Transform3d robotToFLATCamera = new Transform3d(new Translation3d(0.23495,0.29845,0.292),new Rotation3d(0,-Math.PI/6,0));
    //public static final Transform3d robotToFrontObjectCamera = new Transform3d(new Translation3d(0, 0, 0), new Rotation3d(0, 0, 0));
 
